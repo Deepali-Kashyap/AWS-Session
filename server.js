f6cc371da2f0a89e827af78e31cf4fe18427ba9a
Deepali-Kashyap <deeapli.kashyap9718@gmail.com>
2024-12-13 23:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,33 +7,36 @@ const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 const api_key = process.env.SECRET_KEY;
+
 const stripe = require("stripe")(api_key);
 
 // ------------ Imports & necessary things here ------------
 
 // Setting up the static folder:
+// app.use(express.static(resolve(__dirname, "./client")));
 app.use(express.static(resolve(__dirname, process.env.STATIC_DIR)));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Route to serve index.html
 app.get("/", (req, res) => {
   const path = resolve(process.env.STATIC_DIR + "/index.html");
   res.sendFile(path);
 });
 
-// Routes for success and cancel pages
+// creating a route for success page:
 app.get("/success", (req, res) => {
   const path = resolve(process.env.STATIC_DIR + "/success.html");
   res.sendFile(path);
 });
 
+// creating a route for cancel page:
 app.get("/cancel", (req, res) => {
   const path = resolve(process.env.STATIC_DIR + "/cancel.html");
   res.sendFile(path);
 });
 
-// Workshop pages
+// Workshop page routes:
 app.get("/workshop1", (req, res) => {
   const path = resolve(process.env.STATIC_DIR + "/workshops/workshop1.html");
   res.sendFile(path);
@@ -47,9 +50,11 @@ app.get("/workshop3", (req, res) => {
   res.sendFile(path);
 });
 
-// Creating a checkout session
+// ____________________________________________________________________________________
+
 const domainURL = process.env.DOMAIN;
 app.post("/create-checkout-session/:pid", async (req, res) => {
+  
   const priceId = req.params.pid;
   
   const session = await stripe.checkout.sessions.create({
@@ -66,15 +71,14 @@ app.post("/create-checkout-session/:pid", async (req, res) => {
     // allowing the use of promo-codes:
     allow_promotion_codes: true,
   });
-
   res.json({
     id: session.id,
   });
 });
 
 // Server listening:
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
-  console.log(`You may access your app at: ${domainURL}`);
+  console.log(`You may access you app at: ${domainURL}`);
 });
 
